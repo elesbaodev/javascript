0638d22e55c2d86eb529fbac452117b4a525675b
Elesbão Neto <136347846+elesbaodev@users.noreply.github.com>
2023-10-25 22:24:59 -0300

    exercícios feitos

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 40
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 50) {
+    console.log("Parabêns! você está andando na quilometragem correta.")
+} 
+console.log("Dirija sempre usando cinto de segurança para ficar seguro!")
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var país = "América do Sul"
+if (país != "América do Sul") {
+    console.log("Gringo")
+} else {
+    console.log("Latino Americano")
+}
