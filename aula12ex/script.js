62bb63552355927062dc14e6042a92900ea0bfd8
Elesbão Neto <136347846+elesbaodev@users.noreply.github.com>
2023-10-27 13:16:07 -0300

    exercício 01 terminado.

---
+++ aula12ex/script.js
@@ -0,0 +1,22 @@
+
+var horas = new Date()
+var minuts = horas.getMinutes()
+var agora = horas.getHours()
+var horad = window.document.querySelector("div.horass")
+var img01 = window.document.getElementById("img")
+
+if (agora < 12) {
+    document.body.style.background = "rgb(194, 129, 85)"
+    img01.innerHTML = "<img src = 'imagens/diaimg.jpg' width = '250'>"
+    
+} else if (agora > 11 && agora < 19) {
+    document.body.style.background = "rgb(117, 75, 47)"
+    img01.innerHTML = "<img src = 'imagens/tardeimg.jpg' width = '250'>"
+} else if (agora > 18) {
+    document.body.style.background = "rgb(63, 63, 63)"
+    img01.innerHTML = "<img src = 'imagens/noiteimg.jpg' width = '250'>"
+
+}
+
+var pa = window.document.getElementById("p1").innerHTML = `Agora são ${agora}:${minuts} `
+
