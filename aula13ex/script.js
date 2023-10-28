a835703e21ee8e4ac3b809531d1e27921910e2b2
Elesbão Neto <136347846+elesbaodev@users.noreply.github.com>
2023-10-27 22:43:47 -0300

    exercício sendo feito (Verificador de Idade)

---
+++ aula13ex/script.js
@@ -0,0 +1,17 @@
+/*var nas = window.document.getElementById("nasc") + Number(nas.value)
+var num = nas 
+var ano = new Date()
+var anos = ano.getFullYear()
+var res = window.document.getElementById("res")
+var res = anos - num
+window.alert(res)*/
+
+function calcAnos() {
+    var nas = window.document.getElementById("nasc")
+    var num = nas 
+    var ano = new Date()
+    var anos = ano.getFullYear()
+    var res = window.document.getElementById("res")
+    var res = anos - num
+    window.alert(res)
+}
