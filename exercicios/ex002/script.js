84362603b07dee6048bc861f16e8bf7098de37b1
Elesbão Neto <136347846+elesbaodev@users.noreply.github.com>
2023-10-30 11:24:31 -0300

    ...

---
+++ exercicios/ex002/script.js
@@ -0,0 +1,14 @@
+//Cantoras
+
+var d = "Isa"
+var c = "Hebe"
+var c = "Gilda"
+var c = "Júlia"
+var e = "Fátima"
+
+//Cofre de medalhas
+var cofre = 9
+var senha = 4
+var menos_usado = 7
+var mais_usado = 4
+var senha_direita_esquerda = 4
