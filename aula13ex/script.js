0faa3555f9922a20e45d6327acc910a2d3c751d1
Elesbão Neto <136347846+elesbaodev@users.noreply.github.com>
2023-10-29 22:16:20 -0300

    Verificador de idade sendo feito...

--- aula13ex/script.js
+++ aula13ex/script.js
@@ -1,23 +1,16 @@
-
-function calcAnos() {
-    var nas = window.document.getElementById("nasc")
-    var num = Number(nas.value)
-    var ano = new Date()
-    var anos = ano.getFullYear()
-    var res = window.document.getElementById("res")
-    var res = anos - num
-    window.alert(res)
-    var text = window.document.getElementById("text")
-    var masc = window.document.querySelector("input#masc")
-    var fem = window.document.querySelector("input#fem")
-    var sex = window.document.querySelector
-
-    if (sex == masc) {
-        text.innerHTML = "Opa"
-        
-    } else if (sex == fem ) {
-        text.innerHTML = "Oi"
+    function ClicouOn() {
+        var sendd = window.document.getElementById("send")
+        var sex = window.document.getElementsByName("sex")
+        var nas = window.document.getElementById("nasc")
+        var num = Number(nas.value)
+        var ano = new Date()
+        var anos = ano.getFullYear()
+        var sendd = anos - num
+        window.alert(sendd) 
     }
-    
 
-}
+
+
+
+
+
