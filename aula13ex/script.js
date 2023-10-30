a6df724f65e037ef846146a5ba9ec17759f8517e
Elesbão Neto <136347846+elesbaodev@users.noreply.github.com>
2023-10-30 10:56:20 -0300

    ....

--- aula13ex/script.js
+++ aula13ex/script.js
@@ -70,17 +70,6 @@
 
 
 
-        /*var sendd = window.document.getElementById("send")
-        var sex = window.document.getElementsByName("sex")
-        var nas = window.document.getElementById("nasc")
-        var num = Number(nas.value)
-        var ano = new Date()
-        var anos = ano.getFullYear()
-        var sendd = anos - num
-        window.alert(sendd)*/
-    
-
-
 
 
 
