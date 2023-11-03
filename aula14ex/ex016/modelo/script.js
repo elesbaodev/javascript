1d060a58b91cc4e6f819e5a975a05291a048e711
Elesbão Neto <136347846+elesbaodev@users.noreply.github.com>
2023-11-03 16:21:44 -0300

    Desafio 1 proposto terminado com sucesso!

--- aula14ex/ex016/modelo/script.js
+++ aula14ex/ex016/modelo/script.js
@@ -1,19 +1,43 @@
+// Meu código (03/11/23)
 function Contar() {
     var numero01 = window.document.querySelector("input#inicio")
-    var n1 = Number(numero01.value)
     var numero02 = window.document.querySelector("input#fim")
-    var n2 = Number(numero02.value)
     var passo = window.document.querySelector("input#passo")
+    var n1 = Number(numero01.value)
+    var n2 = Number(numero02.value)
     var n3 = Number(passo.value)
-    var cont = window.document.getElementById("contar")
-    var saida = window.document.querySelector("div#saida")
+    var saida = window.document.getElementById("saida")
     var novoHTML = " "
 
-    for(n1 = Number(numero01.value); n1 <= n2; n1++) {
-        novoHTML += n1 + "   "
-
+    if ( n1 || n2 == 0 ) {
+        saida.innerHTML= "Impossível contar!"
     }
-
-    saida.innerHTML = novoHTML
+      if (n1 && n2 > 1 && n3 == 0) {
+        for(n1 = Number(numero01.value); n1 <= n2; n1++) {
+            var cont = "🏴󠁥󠁴󠁳󠁮󠁿"
+           saida.innerHTML =  novoHTML += n1 + "\u{1F449}"
+           
+           
+           
+        } 
+    } else if (n3 >= 1) {
+        for(n1 = Number(numero01.value); n1 <= n2; n1 = n1 + n3) {
+            saida.innerHTML =  novoHTML += n1 + " \u{1F449}"
+        } 
+        saida.innerHTML += "\u{1F3F4}"
+    } 
+    if (n1 > n2 && n3 == 0) {
+        for(n1 = Number(numero01.value); n1 >= n2; n1--) {
+            saida.innerHTML=  novoHTML += n1 + " \u{1F449}"
+         } 
+         saida.innerHTML += "\u{1F3F4}"
+      
+    } else if (n1 > n2 && n3 >= 1) {
+        for(n1 = Number(numero01.value); n1 >= n2; n1 = n1 - n3) {
+            saida.innerHTML =  novoHTML += n1 + "\u{1F449}"
+        } 
+        saida.innerHTML += "\u{1F3F4}"
+    }
+    
     
 }
