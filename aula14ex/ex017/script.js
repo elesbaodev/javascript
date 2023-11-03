6c2aa565b6ab5a37d2c6dce84e9f87377a2ce689
Elesbão Neto <136347846+elesbaodev@users.noreply.github.com>
2023-11-03 17:16:59 -0300

    ...

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,14 @@
+function Tabuada() {
+    let inicio = window.document.querySelector("input#numero01")
+    let n1 = Number(inicio.value)
+    let res = window.document.querySelector("input#gerar")
+    let number = window.document.getElementById("numeros")
+    let HTML = " "
+
+    while (n1 <= 10) {
+        alert(n1)
+        n1++
+    } 
+    
+    
+}
