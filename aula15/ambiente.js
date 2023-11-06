c70b6db432e74228d1aaccf195147630f033b824
Elesbão Neto <136347846+elesbaodev@users.noreply.github.com>
2023-11-06 10:33:33 -0300

    Prática vetores

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,5 +1,11 @@
-let num = [5,1,5]
+let num = [5,8,5]
 num.push(3)
 num[4] = 2
-num.sort()
 console.log(`O vetor ${num} tem ${num.length} posições/elementos.`)
+let pos = num.indexOf(5)
+if (pos == - 1) {
+    console.log("O valor não foi encontrado.")
+}
+else {
+    console.log(`O valor 10 está na posição ${pos}`)
+}
--- aula15/vetornatela.js
+++ aula15/vetornatela.js
@@ -1,8 +1,13 @@
 let num = [5,1,4]
-num.push(3)
-num[4] = 2
+num.push(4)
+num[4] = 6
 num.sort()
-
-for(let cont = 0; cont < num.length; cont++) {
+/*for(let cont = 0; cont < num.length; cont++) {
     console.log(`Posição ${num[cont]}`)
 }  console.log("Ao total " + num.length + " elementos.")
+*/
+
+for(let cont in num) {
+    console.log(`A posição ${cont} tem o valor ${num[cont]}.`)
+    
+}
